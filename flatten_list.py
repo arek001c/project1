4e14c3658c153af21db90861bce3c69f74bbe966
arek001c <164615772+arek001c@users.noreply.github.com>
2024-03-23 14:09:22 +0100

    [cerating new django template with gitigrnore exceptions]

--- flatten_list.py
+++ flatten_list.py
@@ -1,11 +1,10 @@
-def flatten_list(data):
-    result = []
+def flatten_list(data, result=[]):
     for element in data:
-        if isinstance(element, (list,float,str)):
+        if isinstance(element, (float,int, str)):
             result.append(element)
         else:
             flatten_list(element)
     result.sort()
     return result
 
-print(flatten_list([1,[2,3],[5,[2,[8,9]]]]))
+print(flatten_list([1,[2,[5,6,[8,9]]]]))
